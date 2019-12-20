05f852079beefea04b7702e24c7de62f14afe150
louiebloo <laleggio@ucdavis.edu>
2019-12-19 22:08:34 -0700

    Wishlists no longer return claimed user info if owner of list

--- src/wish-list/list/get.js
+++ src/wish-list/list/get.js
@@ -8,29 +8,57 @@ module.exports.validation = [
 ];
 module.exports.handler = async (req, res, next) => {
 
+  //allow the GET request to handle a single wishlist or all wishlists for a user
   let filter = {};
-  if(req.validParams.id){
+  if (req.validParams.id) {
     filter._id = req.validParams.id;
   }
-  if(req.validParams.owner){
+  if (req.validParams.owner) {
     filter.owner = req.validParams.owner;
   }
 
-  if(Object.keys(filter).length === 0 && filter.constructor === Object){
-    throw({status:400,error:"No filter"})
+  //make sure we have at least 1 thing we are filtering by
+  if (Object.keys(filter).length === 0 && filter.constructor === Object) {
+    throw ({ status: 400, error: "No filter" })
   }
 
   let targetWishList = await ListModel.find(filter)
-  .populate('owner',{
-    name: 1,
-  })
-  .populate({path:'items',populate:{path:'claimedUser',select:{'name':1}}})
-  .then(async(data, err) => {
-    if (err) {
-      throw ({ error: err })
-    }
-    return data;
+    .populate('owner', {
+      name: 1,
+    })
+    .populate(
+      {
+        path: 'items',
+        populate: {
+          path: 'claimedUser',
+          select: {
+            'name': 1
+          }
+        }
+      })
+    .lean()
+    .then(async (data, err) => {
+      if (err) {
+        throw ({ error: err })
+      }
+      return data;
   });
+  
+  if(targetWishList != null && targetWishList.length > 0){
+    removeClaimedUserFromResponse(targetWishList,req.credentials._id)
+  }
 
-  return {status:200,response:targetWishList}
+  return { status: 200, response: targetWishList }
+}
+
+//Dont return any information about who has claimed an item if the list belongs to this user
+const removeClaimedUserFromResponse = function(wishlists,ownerId){
+  wishlists.forEach(list=>{
+    if(list.owner._id == ownerId){
+      list.items.forEach(item=>{
+        delete(item.claimedUserMessage);
+        delete(item.claimedUser);
+      })
+    }
+  })
 }
