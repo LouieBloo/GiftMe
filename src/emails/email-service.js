3358393ac5f7585e4d54ed2e72c6e4c1d6631f24
domleggio <domleggio@gmail.com>
2020-12-02 22:47:35 -0800

    GIM-11 added sendgrid email service

---
+++ src/emails/email-service.js
@@ -0,0 +1,33 @@
+const sgMail = require('@sendgrid/mail'); 
+const config = require('../../config/main-app-config')
+sgMail.setApiKey(config.sendgrid.key);
+
+const templateIds = {
+  listUpdateEmailId: "d-f76fb236ed934f49be4fc6df86dbe006"
+}
+const from = "admin@gimmie.gifts"
+
+module.exports.templateIds = templateIds;
+
+module.exports.sendEmail = async (to, subject, templateId, dynamicTemplateData = null) => {
+
+  //to is an array of strings
+  if(!to){
+    return
+  }
+  const toArray = Array.isArray(to) ? to : [to];
+  const personalizations = [];
+
+  toArray.map(emailAddress => {
+    personalizations.push({to: [{email:emailAddress}]})
+  })
+
+  const msg = {
+    personalizations: personalizations,
+    from: from,
+    subject: subject,
+    template_id: templateId,
+    dynamic_template_data: dynamicTemplateData,
+  };
+  return await sgMail.send(msg)
+}
