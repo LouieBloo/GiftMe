80c9de663830f34c4740cf5dab809dd04475b324
Luke <laleggio@ucdavis.edu>
2020-12-13 00:17:33 -0800

    Adding delete notification

--- src/wish-list/list/item/delete.js
+++ src/wish-list/list/item/delete.js
@@ -3,6 +3,7 @@ const ObjectId = mongoose.Types.ObjectId;
 const ItemModel = mongoose.model('wishlist_item');
 const ListModel = mongoose.model('wishlist');
 const { check } = require('express-validator');
+const notificationService = require('../../../notifications/notification-service');
 
 module.exports.validation = [
   check('id').trim().isLength({ min: 2, max: 60 }).withMessage("Id must be 2-60 characters long"),
@@ -16,11 +17,18 @@ module.exports.handler = async (req, res, next) => {
   targetList.items.pull(new ObjectId(req.validParams.id))
   await targetList.save();
 
+  //get the item so notifiaction service can use it
+  let targetItem = await ItemModel.findOne({_id: req.validParams.id});
+  targetItem = targetItem.toObject();
+
   //delete it
   await ItemModel.deleteOne({
     owner: req.credentials._id,
     _id: req.validParams.id
   })
+
+  //tell notification service we have deleted an item
+  notificationService.itemDeleted(targetItem,targetList);
 
   return { status: 200, response: {} }
 }
