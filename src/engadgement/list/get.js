06d342e4aad88966b9d0edc86526556b5ede8dec
Luke <laleggio@ucdavis.edu>
2020-12-22 19:09:18 -0800

    Fixing recent list list order

--- src/engadgement/list/get.js
+++ src/engadgement/list/get.js
@@ -14,18 +14,21 @@ module.exports.handler = async (req, res, next) => {
       '$match': {
         'user': new ObjectId(req.credentials._id)
       }
-    }, {
+    },
+    {
       '$group': {
         '_id': '$list',
         'dateCreated': {
           '$max': '$dateCreated'
         }
       }
-    }, {
+    },
+    {
       '$sort': {
         'dateCreated': -1
       }
-    }, {
+    },
+    {
       '$limit': req.validParams.limit
     }
   ]).then(async (data, err) => {
@@ -36,27 +39,47 @@ module.exports.handler = async (req, res, next) => {
       return [];
     }
 
-    let wishListIds = data.map(element => {
+    let ids = data.map(element => {
       return new ObjectId(element._id);
     });
+
 
     //we need to do another query to make it easier for us
     let wishlists = await WishlistModel.find({
       _id: {
-        $in: wishListIds
+        $in: ids
       }
-    },{ 
+    }, {
         _id: 1,
         finishDate: 1,
         name: 1,
-        items:1
-    }).populate('owner', {
-      name: 1,
-    }).populate('items',{
-      name:1
-    })
+        items: 1
+      }).populate('owner', {
+        name: 1,
+      }).populate('items', {
+        name: 1
+      }).lean()
 
-    return wishlists;
+    //we need to join the lists we fetched into the engagements to retain order
+    let finalWishLists = [];
+    data.forEach(engagement => {
+      let returnObj = {};
+      for(let x = 0; x < wishlists.length; x++){
+        if(engagement._id.toString() == wishlists[x]._id.toString()){
+          returnObj = wishlists[x];
+          break;
+        }
+      }
+      //in case the engagment list was deleted
+      if(returnObj._id){
+        returnObj.engagement = {
+          dateCreated:engagement.dateCreated
+        }
+        finalWishLists.push(returnObj)
+      }
+    });
+
+    return finalWishLists;
   });
 
   return { status: 200, response: wishListIds }
